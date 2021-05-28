0be28919f247bb7f4b90d860a9a053789c0a0e64
Hartmut Ott <mrbraveheart71@gmail.com>
2021-05-28 12:40:57 -0400

    fix

--- test/reflect.js
+++ test/reflect.js
@@ -1,10 +1,9 @@
 const Reflect = artifacts.require("REFLECT");
 
-contract('Relect', (accounts) => {
+contract('Reflect', (accounts) => {
   it('should put 10000 Reflect in the first account', async () => {
     const ReflectInstance = await Reflect.deployed();
     const balance = await ReflectInstance.balanceOf.call(accounts[0]);
-    print(balance.valueOf)
-    assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+    assert.equal(balance.valueOf(), 100000000000, "10000 wasn't in the first account");
   });
 });
