990a24232af55ab3de8096af6a8065702fa22cab
Hartmut Ott <mrbraveheart71@gmail.com>
2021-05-28 12:23:13 -0400

    fix

--- test/reflect.js
+++ test/reflect.js
@@ -1,10 +1,10 @@
 const Reflect = artifacts.require("REFLECT");
 
-contract('MetaCoin', (accounts) => {
+contract('Relect', (accounts) => {
   it('should put 10000 Reflect in the first account', async () => {
     const ReflectInstance = await Reflect.deployed();
     const balance = await ReflectInstance.balanceOf.call(accounts[0]);
-
+    print(balance.valueOf)
     assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
   });
 });
